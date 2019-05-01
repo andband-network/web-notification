05c5e076f6141f6622bd1470ee3ab899b5b8fe6e
andband <finnhoulihan@email.com>
2019-05-01 23:13:40 +0100

    connection notification

--- src/main/java/com/andband/notification/web/NotificationService.java
+++ src/main/java/com/andband/notification/web/NotificationService.java
@@ -24,7 +24,7 @@ public class NotificationService {
     void userRegistration(NotificationRequest request) {
         EmailContent emailContent = emailContentRepository.findByType(EmailType.REGISTRATION);
 
-        String userName = request.getUserTo();
+        String userName = request.getToProfileName();
 
         String tokenString = request.getText();
         String confirmRegistrationUrl = runtimeProperties.getConfirmRegistrationUrl();
@@ -32,42 +32,41 @@ public class NotificationService {
 
         String body = String.format(emailContent.getBody(), userName, tokenUrl);
 
-        sendEmail(request.getEmail(),  emailContent.getSubject(), body);
+        sendEmail(request.getEmail(), emailContent.getSubject(), body);
     }
 
     void profileMessage(NotificationRequest request) {
         EmailContent emailContent = emailContentRepository.findByType(EmailType.PROFILE_MESSAGE);
 
-        String userTo = request.getUserTo();
-        String userFrom = request.getUserFrom();
+        String userTo = request.getToProfileName();
+        String userFrom = request.getFromProfileName();
         String messageText = request.getText();
 
         String subject = String.format(emailContent.getSubject(), userFrom);
         String body = String.format(emailContent.getBody(), userTo, userFrom, messageText);
 
         sendEmail(request.getEmail(), subject, body);
-   }
+    }
 
     void connectionRequest(NotificationRequest request) {
-        sendConnectionNotification(request);
+        sendConnectionNotification(request, EmailType.CONNECTION_REQUEST);
     }
 
     void connectionConfirmed(NotificationRequest request) {
-        sendConnectionNotification(request);
+        sendConnectionNotification(request, EmailType.CONNECTION_CONFIRMED);
     }
 
-    private void sendConnectionNotification(NotificationRequest request) {
-        EmailContent emailContent = emailContentRepository.findByType(EmailType.PROFILE_MESSAGE);
+    private void sendConnectionNotification(NotificationRequest request, EmailType emailType) {
+        EmailContent emailContent = emailContentRepository.findByType(emailType);
 
-        String userTo = request.getUserTo();
-        String userFrom = request.getUserFrom();
+        String userTo = request.getToProfileName();
+        String userFrom = request.getFromProfileName();
 
         String subject = String.format(emailContent.getSubject(), userFrom);
         String body = String.format(emailContent.getBody(), userTo, userFrom);
 
         sendEmail(request.getEmail(), subject, body);
     }
-
 
     private void sendEmail(String email, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
